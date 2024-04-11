c58eeb8ed264d001e1c7d1c8582f1973cdd50880
BearNord <bor.bernard@gmail.com>
2024-04-11 07:51:52 +0000

    Addad environment yml

--- code/TFC/run_exp.sh
+++ code/TFC/run_exp.sh
@@ -1,6 +1,3 @@
-python main.py --training_mode pre_train --pretrain_dataset SleepEEG --target_dataset Epilepsy --seed 420
-python main.py --training_mode pre_train --pretrain_dataset SleepEEG FD_A --target_dataset Epilepsy --seed 420
-python main.py --training_mode pre_train --pretrain_dataset SleepEEG FD_A HAR --target_dataset Epilepsy --seed 420
 python main.py --training_mode pre_train --pretrain_dataset SleepEEG FD_A HAR ECG --target_dataset Epilepsy --seed 420
 
 python main.py --training_mode fine_tune_test --pretrain_dataset SleepEEG --target_dataset Epilepsy --seed 42
